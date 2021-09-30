01d186ac712eb839a06e8c2ee38949412cca9067
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2021-10-01 00:58:17 +0300

    feat: add tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,18 +3,27 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+
+  const turns = Math.pow(2, disksNumber)-1
+  const turnsSpeedSec = turns / (turnsSpeed/60/60)
+
+  return {
+    turns: turns,
+    seconds: Math.floor(turnsSpeedSec)
+  }
   // remove line with error and write your code here
 }
+
+console.log(calculateHanoi(9, 4308))
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,17 +3,61 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  let newArr = []
+  for (let i = 0; i < arr.length; i += 1) {
+
+    switch (arr[i]) {
+      case ('--discard-next'):
+        if (i < arr.length - 2) {
+          i++
+        }
+        break;
+      case ('--discard-prev'):
+        if (arr[i - 2] === '--discard-next') {
+          console.log("string")
+          break
+        }
+        if (i > 0) {
+          newArr.pop()
+        }
+        break;
+      case ('--double-next'):
+        if (i < arr.length - 2) {
+          newArr.push(arr[i + 1])
+        }
+        break;
+      case ('--double-prev'):
+        if (arr[i - 2] === '--discard-next') {
+          console.log("string")
+
+          break
+        }
+        if (i > 0) {
+          newArr.push(arr[i - 1])
+        }
+        break;
+      default:
+        newArr.push(arr[i])
+        break;
+    }
+  }
+  return newArr
 }
+
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]))
